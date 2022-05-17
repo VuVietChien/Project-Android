9c1ddfc4fcfcf558eaa382541e568956d3841443
datnguyen2001 <nguyendatdz01@gmail.com>
2022-05-17 21:40:21 +0700

    ok

---
+++ app/src/main/java/com/example/appbanhang/activity/splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.appbanhang.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.appbanhang.R;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashscreen.this,MainActivity.class));
+            }
+        },2000);
+    }
+}
