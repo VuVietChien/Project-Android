2447673d7384d098fdbee0d7a0175475efc20c29
Nguyễn Minh Thuận <65107294+thuanbin1101@users.noreply.github.com>
2022-06-07 22:57:01 +0700

    OK

--- app/src/main/java/com/example/appbanhang/ultil/server.java
+++ app/src/main/java/com/example/appbanhang/ultil/server.java
@@ -3,8 +3,6 @@ package com.example.appbanhang.ultil;
 public class server {
 
     public static String localhost="192.168.1.11";
-
-
     public static String DuongdanLoaisp="http://" + localhost + "/projectandroid/getloaisp.php";
     public static String Duongdansanphammoinhat="http://" + localhost + "/projectandroid/getspmoinhat.php";
     public static String Duongdandienthoai="http://" + localhost + "/projectandroid/getsanpham.php?page=";
